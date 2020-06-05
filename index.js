a880dc0bebf3bd2c7d5afeba19eaa7cd63d64f2d
JaiTheGuy <kevinxj40@gmail.com>
2020-06-04 23:02:14 -0400

    Added Dice Roll and Fortune Cookie Commands

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const { prefix, age, bot_info } = require('./config.json');
+const { cookieReadings } = require('./fortuneCookie.json');
 
 client.once('ready', () => {
   console.log('Success!');
+  console.log(prefix);
+  console.log(age);
+  console.log(bot_info);
 });
 
+const fortunes = {
+  cookieReadings: ['yo', 'yooo', 'yoooooo', 'oernfo', 'nfkwnfkwnejf'],
+};
+
+const diceRoll = () => Math.floor(Math.random() * 6) + 1;
+const fortuneCookie = () =>
+  fortunes.cookieReadings[Math.floor(Math.random() * 2) + 1];
+
 client.on('message', (message) => {
-  if (message.content === '!hello') {
+  if (message.author.bot) {
+    return;
+  }
+  if (
+    message.content === `${prefix}hi` ||
+    message.content === `${prefix}hello`
+  ) {
     message.channel.send(
       `Hello ${message.author.username}, how are you doing today?`
     );
+  }
+  if (
+    message.content === `${prefix}goodbye` ||
+    message.content === `${prefix}bye`
+  ) {
+    message.channel.send(`See you later ${message.author.username}!`);
+  }
+  if (message.content === `${prefix}rolldice`) {
+    message.reply(`you rolled a ` + diceRoll() + '!');
+  }
+  if (message.content === `${prefix}cookie`) {
+    message.reply(fortuneCookie());
   }
 });
